fc61459cac9948776a037a6f73054ef3aa358c89
Adrien Guillo <adrien.guillo@gmail.com>
2015-08-23 19:29:51 -0400

    Add packaging

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+
+setup(name='thrashtide',
+      packages=find_packages(exclude=['tests']),
+      version='0.1.0',
+      zip_safe=False,
+      )
--- thrashtide/config.py
+++ thrashtide/config.py
@@ -1,1 +1,5 @@
-STATIONS_PATH = "resources/stations.json"
+import os
+
+
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+STATIONS_PATH = os.path.join(ROOT_DIR, "stations.json")
